f2901460c8cb4176c63f5bce6f839fd95adea951
Andy Hanson <anhans@microsoft.com>
2018-03-12 13:38:17 -0700

    Store import nodes in SourceFile.imports instead of StringLiteral nodes

--- src/services/services.ts
+++ src/services/services.ts
@@ -646,7 +646,7 @@ namespace ts {
         public nameTable: UnderscoreEscapedMap<number>;
         public resolvedModules: Map<ResolvedModuleFull>;
         public resolvedTypeReferenceDirectiveNames: Map<ResolvedTypeReferenceDirective>;
-        public imports: StringLiteral[];
+        public imports: ReadonlyArray<AnyValidImport>;
         public moduleAugmentations: StringLiteral[];
         private namedDeclarations: Map<Declaration[]>;
         public ambientModuleNames: string[];
